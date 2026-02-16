29288bde8aa6a89f9905ace8076a6e43fb2168fb
SimoneZoroddu <lenviur@gmail.com>
2026-02-16 16:52:17 +0100

    countdown 30s with adding and removing number and input

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
 const numbersListEl = document.getElementById('numbers-list')
+const countDownEl = document.getElementById('countdown')
+const answersFormEl = document.getElementById('answers-form')
 //console.log(numbersListEl);
 
 
-for (let i = 0; i <= 5; i++) {
+
+for (let i = 0; i < 5; i++) {
     const liEl = document.createElement('li')
     numbersListEl.appendChild(liEl)
     liEl.innerText = Math.floor(Math.random() * 20 + 1)
 }
+
+let count = 30
+const countDown = setInterval(function () {
+    if (count === -1) {
+        clearInterval(countDown)
+        numbersListEl.classList.add('d-none')
+        countDownEl.classList.add('d-none')
+        answersFormEl.classList.remove('d-none')
+    } else {
+        countDownEl.innerText = count--
+    }
+}, 200) // e da settare a 1000 cosi si vede per ogni secondo
